2147e62a6369837e3d69734e983afd10d1447d5a
luca.sbardella <luca.sbardella@66bd7c67-aed3-ea1c-df33-5c639bee51e2>
2010-06-18 14:48:02 +0000

    added test for 'self' ForeignKey

--- trunk/stdnet/tests/__init__.py
+++ trunk/stdnet/tests/__init__.py
@@ -2,4 +2,5 @@
 #from backend_tests import *
 
 from tables import *
+from fktest import *
 from stdnet.contrib.timeserie.tests import *
--- trunk/stdnet/utils/anyjson.py
+++ trunk/stdnet/utils/anyjson.py
@@ -1,4 +1,18 @@
+import time
+from datetime import date, datetime
 try:
     import json
 except:
     import simplejson as json
+    
+date2timestamp = lambda dte : int(time.mktime(dte.timetuple()))
+    
+class JSONRPCEncoder(json.JSONEncoder):
+    """
+    Provide custom serializers for JSON-RPC.
+    """
+    def default(self, obj):
+        if isinstance(obj, date) or isinstance(obj, datetime):
+            return date2timestamp(obj)
+        else:
+            raise exceptions.JSONEncodeException("%r is not JSON serializable" % (obj,))
