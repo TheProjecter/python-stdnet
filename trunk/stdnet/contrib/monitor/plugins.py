3c501496cc8d9cb8da2f2acff4721a5ca28d797b
luca.sbardella <luca.sbardella@66bd7c67-aed3-ea1c-df33-5c639bee51e2>
2010-10-20 17:04:05 +0000

    improved redis monitor plugin

--- trunk/stdnet/contrib/monitor/plugins.py
+++ trunk/stdnet/contrib/monitor/plugins.py
@@ -7,6 +7,9 @@ from django.utils.dateformat import format, time_format
 from djpcms.template import loader
 from djpcms.plugins import DJPplugin
 
+from stdnet import orm
+from stdnet.utils.format import format_number
+
 import redis
 
 
@@ -56,7 +59,14 @@ def nicetimedelta(t):
         return u'about %s days' % days
 
 
+def getint(v):
+    try:
+        return int(v)
+    except:
+        return None
+
 class RedisMonitorPlugin(DJPplugin):
+    '''Plugin which displays redis server status.'''
     form = RedisForm
     description = 'Redis monitor'
     
@@ -68,13 +78,36 @@ class RedisMonitorPlugin(DJPplugin):
         r = redis.Redis(host = server, port = port, db = db)
         info = r.info()
         info1 = []
+        info2 = []
+        model_info = []
+        keys = 0             
+        for k in info:
+            if k[:2] == 'db':
+                num = getint(k[2:])
+                if num is not None:
+                    data  = info[k]
+                    keydb = data['keys']
+                    data['keys'] = format_number(keydb)
+                    niceadd(info2, 'database %s' % num, 'keys %(keys)s, expires %(expires)s' % data)
+                    keys += keydb
         niceadd(info1, 'Redis version', info['redis_version'])
         niceadd(info1, 'Process id', info['process_id'])
+        niceadd(info1, 'Total keys', format_number(keys))
         niceadd(info1, 'Memory used', info['used_memory_human'])
         niceadd(info1, 'Up time', nicetimedelta(info['uptime_in_seconds']))
         niceadd(info1, 'Virtual Memory enabled', 'yes' if info['vm_enabled'] else 'no')
         niceadd(info1, 'Last save', nicedate(info['last_save_time']))
-        niceadd(info1, 'Commands processed', info['total_commands_processed'])
-        niceadd(info1, 'Connections received', info['total_connections_received'])
+        niceadd(info1, 'Commands processed', format_number(info['total_commands_processed']))
+        niceadd(info1, 'Connections received', format_number(info['total_connections_received']))
+        
+        model_header = ['name','db','base key']
+        for model in orm.mapper._registry:
+            meta = model._meta
+            cursor = meta.cursor
+            if cursor.name == 'redis':
+                model_info.append([meta, cursor.db, meta.basekey()])
         return loader.render_to_string('monitor/redis_monitor.html',
-                                       {'info1':info1})
+                                       {'info1':info1,
+                                        'info2':info2,
+                                        'model_header':model_header,
+                                        'model_info':model_info})
